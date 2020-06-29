babe28333ea6cf92724aabce1ac63f957aa66be3
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-06-29 03:20:42 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,32 @@
+  
 class BankAccount
+  attr_reader :account, :name
+  attr_accessor :balance, :status
 
+  def initialize(account)
+    @account = account
+    @name = account
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(value)
+    @balance += value
+  end
+
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+
+  def valid?
+    if self.status == "open" && self.balance > 0
+      true
+    else
+      false
+    end
+  end
+
+  def close_account
+    @status = "closed"
+  end
 end
